fd4733e990c9d5ae6169c45e00fc62d0b447e7f2
tanwiz13 <jsntsingh25@gmail.com>
2025-03-11 19:57:21 +0530

    project init and handling 0 and multiple numbers in add

---
+++ StringCalculator.js
@@ -0,0 +1,9 @@
+class StringCalculator {
+  add(numbers) {
+    if (!numbers) return 0;
+    const numArray = numbers.split(',').map(Number);
+    return numArray.reduce((sum, num) => sum + num, 0);
+  }
+}
+
+module.exports = StringCalculator;
