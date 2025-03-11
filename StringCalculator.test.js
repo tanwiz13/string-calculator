c56921320db509797fa16c63c98d3b949bdd6d0e
tanwiz13 <jsntsingh25@gmail.com>
2025-03-11 20:03:56 +0530

    added test cases for empty, single and two numbers

---
+++ StringCalculator.test.js
@@ -0,0 +1,21 @@
+const StringCalculator = require("./StringCalculator");
+
+describe("StringCalculator", () => {
+  let calculator;
+
+  beforeEach(() => {
+    calculator = new StringCalculator();
+  });
+
+  test("should return 0 for an empty string", () => {
+    expect(calculator.add("")).toBe(0);
+  });
+
+  test("should return the number itself if only one number is provided", () => {
+    expect(calculator.add("1")).toBe(1);
+  });
+
+  test("should return the sum of two numbers", () => {
+    expect(calculator.add("1,5")).toBe(6);
+  });
+});
