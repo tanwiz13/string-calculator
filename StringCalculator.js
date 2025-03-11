23c74e4807bbfe4f4e82147e24d2ecc54a6acb06
tanwiz13 <jsntsingh25@gmail.com>
2025-03-11 20:11:34 +0530

    added new line delimiter handling in add

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,7 +1,8 @@
 class StringCalculator {
   add(numbers) {
     if (!numbers) return 0;
-    const numArray = numbers.split(',').map(Number);
+    const delimiter = /,|\n/;
+    const numArray = numbers.split(delimiter).map(Number);
     return numArray.reduce((sum, num) => sum + num, 0);
   }
 }
