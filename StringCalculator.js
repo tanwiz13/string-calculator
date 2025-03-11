18cb56e1f575188751212aced1c73a292ab08da7
tanwiz13 <jsntsingh25@gmail.com>
2025-03-11 20:21:44 +0530

    added handling and test for custom delimiters

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,7 +1,12 @@
 class StringCalculator {
   add(numbers) {
     if (!numbers) return 0;
-    const delimiter = /,|\n/;
+    let delimiter = /,|\n/;
+    if (numbers.startsWith("//")) {
+      const match = numbers.match(/^\/\/(.+)\n(.*)/);
+      delimiter = new RegExp(match[1]);
+      numbers = match[2];
+    }
     const numArray = numbers.split(delimiter).map(Number);
     return numArray.reduce((sum, num) => sum + num, 0);
   }
--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -22,4 +22,8 @@ describe("StringCalculator", () => {
   test("should handle new lines as delimiters", () => {
     expect(calculator.add("1\n2,3")).toBe(6);
   });
+
+  test("should support custom delimiters", () => {
+    expect(calculator.add("//;\n1;2")).toBe(3);
+  });
 });
